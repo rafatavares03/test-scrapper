557d97e4e8a714e159bccce2a89c437697769ee5
Rafael Tavares <rafatavares03@hotmail.com>
2025-04-16 01:06:22 -0300

    adicionando muitas noticias

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-const g1URL = "https://g1.globo.com/politica/"
 
 async function autoScroll(page) {
     let previousHeight = await page.evaluate('document.body.scrollHeight');
@@ -25,36 +24,42 @@ async function autoScroll(page) {
 async function start() {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
-    await page.goto(g1URL)
-    
-    await autoScroll(page);
+    for(let pagina = 1; pagina <= 10; pagina++) {
+      let g1URL = `https://g1.globo.com/politica/index/feed/pagina-${pagina}.ghtml`
+      await page.goto(g1URL)
 
-    const link = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".feed-post-link")).map(x => x.getAttribute("href"))
-    })
-    for(let i = 0; i < link.length; i++) {
-        await page.goto(link[i])
-        let dict = await page.evaluate(() => {
-            const dict = {}
-            let manchete = document.querySelector("h1.content-head__title")
-            let lide = document.querySelector("h2.content-head__subtitle")
-            let dataPublicacao = document.querySelector('time[itemprop="dateModified"]')
-            let artigo = Array.from(document.querySelectorAll("article[itemprop='articleBody'] .content-text")).map(x => x.textContent)
-            if(manchete != null) dict.manchete = manchete.textContent
-            if(lide != null) dict.lide = lide.textContent
-            if(dataPublicacao != null) dict.dataPublicacao = dataPublicacao.getAttribute("datetime")
-            if(artigo != null && artigo.length != 0) dict.artigo = artigo
-            return dict
-        })
-        const dictString = JSON.stringify(dict, null, '\t')
-        fs.writeFile(`jsons/noticia${i+1}.json`, dictString, function (err, result){
-            if(err) {
-                console.log(err)
-            } else {
-                console.log("Arquivo criado!")
-            }
-        })
+      const link = await page.evaluate(() => {
+          return Array.from(document.querySelectorAll(".feed-post-link")).map(x => x.getAttribute("href"))
+      })
+      for(let i = 0; i < link.length; i++) {
+          await page.goto(link[i])
+          let dict = await page.evaluate(() => {
+              const dict = {}
+              let manchete = document.querySelector("h1.content-head__title")
+              let lide = document.querySelector("h2.content-head__subtitle")
+              let dataPublicacao = document.querySelector('time[itemprop="dateModified"]')
+              let artigo = Array.from(document.querySelectorAll("article[itemprop='articleBody'] .content-text")).map(x => x.textContent)
+              if(manchete != null) dict.manchete = manchete.textContent
+              if(lide != null) dict.lide = lide.textContent
+              if(dataPublicacao != null) dict.dataPublicacao = dataPublicacao.getAttribute("datetime")
+              if(artigo != null && artigo.length != 0) dict.artigo = artigo
+              return dict
+          })
+          dict.portal = "g1"
+          dict.link = link[i]
+          const dictString = JSON.stringify(dict, null, '\t')
+          fs.writeFile(`jsons/noticia${(pagina*link.length)+i+1}.json`, dictString, function (err, result){
+              if(err) {
+                  console.log(err)
+              } else {
+                  console.log("Arquivo criado!")
+              }
+          })
+      }
     }
+
+    //await autoScroll(page);
+
     await browser.close()
 }
 
