13189fe49a1adb535a29107db27d6ede0c4afee3
Rafael Tavares <rafatavares03@hotmail.com>
2025-04-23 22:04:52 -0300

    alinhando arquivos

--- cnn.js
+++ cnn.js
@@ -1,22 +1,20 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-const { stringify } = require('querystring')
 
 async function cnnScrap() {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.goto("https://www.cnnbrasil.com.br/politica/", { waitUntil: "domcontentloaded" })
 
   let clickCount = 0;
   const maxClicks = 30;
-  /*
+  
   while (clickCount < maxClicks) {
     try {
-      //await autoScroll(page);
-      //await page.waitForSelector('.block-list-get-more-btn', { timeout: 3000 });
-      //await page.click('.block-list-get-more-btn');
-      //await page.waitForTimeout(1000);
-      botao.click()
+      await autoScroll(page);
+      await page.waitForSelector('.block-list-get-more-btn', { timeout: 3000 });
+      await page.click('.block-list-get-more-btn');
+      await page.waitForTimeout(1000);
       clickCount++;
     } catch (e) {
       console.log("achou porra nenhuma")
@@ -24,47 +22,17 @@ async function cnnScrap() {
       break;
     }
   }
-    */
+  
 
   // Coleta os links das notícias
-  for(let i = 0; i <= 1000; i++) { //tem que pegar 500, na teoria
-
-    // await page.evaluate(() => {
-    //   window.scrollTo(0, document.body.scrollHeight);
-    // });
-
-    await page.evaluate((i) => {
-      const btn = document.querySelector('.block-list-get-more-btn');
-      if (btn) {
-        btn.setAttribute('data-perpage', '100');
-        btn.setAttribute('data-page-block-list', `${i}`);
-        btn.click();
-      }
-    }, i); 
-    
-
-    
-    // await page.waitForSelector('.block-list-get-more-btn', { visible: true });
-    
-    // await page.evaluate(() => {
-      //   document.querySelector('.block-list-get-more-btn')?.click();
-      // });
-      // console.log(`contador : ${i}`)
-    } 
-    await page.evaluate(() => {
-      window.scrollTo(0, document.body.scrollHeight);
-      });
-    await new Promise(resolve => setTimeout(resolve, 1000)); // pra desencargo  
-
   const links = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("a.home__list__tag")).map(el => el.getAttribute("href"))
+    return Array.from(document.querySelectorAll("a.home__list__tag")).map(el => el.href)
   })
 
-  console.log(links.length)
-  // // Imprime os links
-  // for (let i = 0; i < links.length; i++) {
-  //   console.log(links[i])
-  // }
+  // Imprime os links
+  for (let i = 0; i < links.length; i++) {
+    console.log(links[i])
+  }
 
   await browser.close()
 }
@@ -87,6 +55,6 @@ async function autoScroll(page) {
     }, 100)
       })
     })
-}
-
-cnnScrap()
+  }
+  
+  cnnScrap()
