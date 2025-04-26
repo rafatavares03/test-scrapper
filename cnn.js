c86798364ef2353cf7374ed5996b8732c485c874
hugostoso1 <hugoaasouza45@gmail.com>
2025-04-26 18:03:22 -0300

    sojddvnwadvo

--- cnn.js
+++ cnn.js
@@ -40,27 +40,6 @@ async function coletaDadosCNN(pagina, link) {
   })
 }
 
-async function clicaBotao(page, qtdLinksAtual) {
-  let links = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll("a.home__list__tag")).map(el => el.getAttribute("href"))
-  })
-  try {
-    let clickResult = await page.locator('button.block-list-get-more-btn').click({count: 2 ,delay: 1000})
-    console.log(clickResult)
-  } catch (e) {
-      console.log("Não foi possível carregar novos conteúdos")
-      console.log(e)
-      return null
-  }
-  while(links.length <= qtdLinksAtual) {
-    console.log(links.length)
-    links = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll("a.home__list__tag")).map(el => el.getAttribute("href"))
-    })
-  }
-  return links
-}
-
 async function cnnScrap() {
   const browser = await puppeteer.launch({headless:false})
   const page = await browser.newPage()
@@ -93,62 +72,50 @@ async function cnnScrap() {
           console.log("Não foi possível carregar novos conteúdos")
           console.log(e)
           return null
-      }
-
-      
+      }   
   }
-
-  // let linksQtd = links.length
-  // let scrapingPage = await browser.newPage()
-  // for(let i = 0; i < 1; i++) {
-  //   // for(let j = 0; j < links.length; j++) {
-  //   //   let dict = await coletaDadosCNN(scrapingPage, links[j])
-  //   //   //console.log(dict)
-  //   // }
-
-  //   //await page.bringToFront()
-  //   //links = await clicaBotao(page, linksQtd)
-  //   // if(links != null) {
-  //   //   links = links.slice(linksQtd)
-  //   //   linksQtd += links.length
-  //   // }
-  // }
-  // await scrapingPage.close()
-
-
-  // // Coleta os links das notícias
-  // for(let i = 0; i <= 1000; i++) { //tem que pegar 500, na teoria
-
-  //   // await page.evaluate(() => {
-  //   //   window.scrollTo(0, document.body.scrollHeight);
-  //   // });
-
-  //   await page.evaluate((i) => {
-  //     const btn = document.querySelector('.block-list-get-more-btn');
-  //     if (btn) {
-  //       // btn.setAttribute('data-perpage', '1000');  // deixa por enquanto tavares
-  //       btn.setAttribute('data-page-block-list', `${i}`);
-  //       btn.click();
-  //     }
-  //   }, i); 
-    
-  //   await page.evaluate(() => {
-  //     window.scrollTo(0, document.body.scrollHeight);
-  //     });
-
-  //   await new Promise(resolve => setTimeout(resolve, 10)); // pra desencargo  
-  //   } 
-
-
-  // console.log(links.length)
-  // // // Imprime os links
-  // // for (let i = 0; i < links.length; i++) {
-  // //   console.log(links[i])
-  // // }
-
-	    await new Promise(resolve => setTimeout(resolve, 10000)); // pra desencargo  
-
+	
+	await new Promise(resolve => setTimeout(resolve, 10000)); // pra desencargo  
+	
   await browser.close()
 }
 
 cnnScrap()
+
+
+
+
+
+
+
+
+
+// // Coleta os links das notícias
+// for(let i = 0; i <= 1000; i++) { //tem que pegar 500, na teoria
+
+//   // await page.evaluate(() => {
+//   //   window.scrollTo(0, document.body.scrollHeight);
+//   // });
+
+//   await page.evaluate((i) => {
+//     const btn = document.querySelector('.block-list-get-more-btn');
+//     if (btn) {
+//       // btn.setAttribute('data-perpage', '1000');  // deixa por enquanto tavares
+//       btn.setAttribute('data-page-block-list', `${i}`);
+//       btn.click();
+//     }
+//   }, i); 
+	
+//   await page.evaluate(() => {
+//     window.scrollTo(0, document.body.scrollHeight);
+//     });
+
+//   await new Promise(resolve => setTimeout(resolve, 10)); // pra desencargo  
+//   } 
+
+
+// console.log(links.length)
+// // // Imprime os links
+// // for (let i = 0; i < links.length; i++) {
+// //   console.log(links[i])
+// // }
