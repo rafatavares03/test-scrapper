0dd28bd1691e32ae162b0821b8923d8b5146541a
Rafael Tavares <rafatavares03@hotmail.com>
2025-04-26 18:52:37 -0300

    adiconando manchete,lide,data,portal e link no carta capital

--- carta_capital.js
+++ carta_capital.js
@@ -3,7 +3,23 @@ const puppeteer = require('puppeteer')
 async function coletaDadosCartaCapital(pagina, link) {
   await pagina.goto(link)
   return pagina.evaluate(() => {
-    
+    let dados = {}
+    let manchete = document.querySelector("section.s-content__heading h1")
+    let lide = Array.from(document.querySelectorAll("section.s-content__heading p")).map(x => x.textContent)
+    let dataPublicacao = document.querySelector("div.s-content__infos span span")
+    if(manchete) {
+      dados.manchete = manchete.textContent
+    } else {
+      return null
+    }
+    if(lide.length > 0) {
+      lide = lide.filter(x => x.length > 0)
+      dados.lide = lide[0]
+    } 
+    if(dataPublicacao) dados.dataPublicacao = dataPublicacao.textContent.trim()
+    dados.portal = "Carta Capital"
+    dados.link = window.location.href
+    return dados
   })
 }
 
@@ -16,7 +32,10 @@ async function cartaCapitalScraping() {
   let links = await page.evaluate(() => {
     return Array.from(document.querySelectorAll("a.l-list__item")).map(x => x.getAttribute("href"))
   })
-  console.log(links)
+  for(let i = 0; i < links.length; i++) {
+    let noticia = await coletaDadosCartaCapital(page, links[i])
+    console.log(noticia)
+  }
   await browser.close()
 }
 
